2d9c87cf13673c2224556337207cedd2e3f1c7df
Kristján Bjarni <kristjanbjarni@gmail.com>
2022-09-10 22:45:12 +0000

    Update main.lua
    Added automatic cell number

--- src/WIDGETS/AvgBatt/main.lua
+++ src/WIDGETS/AvgBatt/main.lua
@@ -2,17 +2,19 @@ assert(loadScript("/SCRIPTS/UTIL/util.lua"))()
 
 local DEFAULT_TEXTCOLOR = WHITE
 local DEFAULT_DISABLEDCOLOR = DARKGREY
+local ABSOLUTE_MAX_VOLTAGE = 4.4
+local FULL_VOLTAGE = 4.3
 
 local options = {
   { "Batt", SOURCE, 1 },
-  { "Cells", VALUE,1,1,12 },
+  { "Cells", VALUE,0,0,12 },
   { "TextColor", COLOR, DEFAULT_TEXTCOLOR },
   { "DisabledColor", COLOR, DEFAULT_DISABLEDCOLOR },
   { "Shadow", BOOL, 1 }
 }
 
 local function create(zone, options)
-  local widget = { zone=zone, options=options, last_batt = 0, active = false}
+  local widget = { zone=zone, options=options, last_batt = 0, max_batt = 0, active = false}
   if (widget.options.Batt == nil) then
     local id = 1
     local b = getFieldInfo("VFAS")
@@ -25,7 +27,7 @@ local function create(zone, options)
     widget.options.Batt = id
   end
   if (widget.options.Cells == nil) then
-    widget.options.Cells = 1
+    widget.options.Cells = 0
   end
   if (widget.options.TextColor == nil) then
     widget.options.TextColor = DEFAULT_TEXTCOLOR
@@ -48,6 +50,9 @@ local function updateBattValue(widget)
   local active = (value and value>0 and value<60)
   if active then
     widget.last_batt = value
+    if (value > widget.max_batt) then
+      widget.max_batt = value
+    end
   end
   widget.active = active
 end
@@ -60,13 +65,23 @@ function refresh(widget)
   updateBattValue(widget)
   local color = widget.options.TextColor
   local shadow = widget.options.Shadow == 1
-  local cells = widget.options.Cells or 1
+  local cells = widget.options.Cells or 0
   local zone = getZone(widget.zone)
   local value = widget.last_batt
+  if (cells==0) then
+    cells = round(widget.max_batt / FULL_VOLTAGE,0)
+    if (cells == 0) then
+      cells = 1
+    end  
+    local check_voltage = widget.max_batt / cells
+    if (check_voltage > ABSOLUTE_MAX_VOLTAGE) then
+      cells = cells + 1
+    end
+  end
   if not widget.active then
     color = widget.options.DisabledColor
   end
-  value = round(value / cells,2)
+  value = round(value / cells,2)  
   value = string.format("%.2f",value)
   drawValueZone(widget.zone,"Batt",value,color,shadow)
 end
